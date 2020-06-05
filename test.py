e9b8ea19c7bb4fefa0cd5a0c2c87c99ab773df4d
alexcesi <57102377+alexcesi@users.noreply.github.com>
2020-06-05 13:46:58 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -10,18 +10,22 @@ app = Flask(__name__)
 def home():
 
   random_item = random.randint(1,30)
-  
   prixuser = 0
-  historique = []
   firstry = 0
+  historique = []
+  
   if request.method == "POST": 
     random_item = int(request.form["random_item"])
     prixuser = int(request.form["prixinput"])
+
     historique = json.loads(request.form["historique"])
     historique.append(prixuser)
+
     firstry = float(request.form["firstry"])
     if firstry == 0 :
        firstry = time.time()
+
+
 
   params = {
             "ApiKey": "818e864c-7f59-41db-8546-6498f3d90ef0",
@@ -44,6 +48,8 @@ def home():
       
   url = "https://api.cdiscount.com/OpenApi/json/Search"
  
+
+
   r = requests.post(url, data=json.dumps(params))
   name =(r.json()['Products'][0]['Name'])
   prixobjet = int(float(r.json()['Products'][0]['BestOffer']['SalePrice']))
@@ -53,7 +59,7 @@ def home():
   if request.method == "POST": 
     temps = datetime.datetime.now() - datetime.datetime.fromtimestamp(firstry)
 
-  return render_template("projet.html", NAME=name, PRIXOBJET=prixobjet, IMAGE=image, PRIXUSER=prixuser, RANDOM_ITEM=random_item, HISTORIQUE=historique, FIRSTRY=firstry, TEMPS=temps)
+  return render_template("projet.html", NAME=name, PRIXOBJET=prixobjet, RANDOM_ITEM=random_item, PRIXUSER=prixuser, IMAGE=image,  HISTORIQUE=historique, FIRSTRY=firstry, TEMPS=temps)
 
 if __name__ == "__main__":
     app.run()
